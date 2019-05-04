84603474771221a897d4d02ad058327348a9d326
Bryna Dunnells <badunnells@gmail.com>
2019-05-04 20:19:25 +0000

    dont yield the whole block, just the good parts

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,24 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    new_collection = []
+    while  i < collection.length
+       if yield(collection[i])
+      new_collection << collection[i]
+
+   end
+   i +=1
+   end# code here
+   new_collection
+end
+
+
+def my_collect(array) # put argument(s) here
+   i = 0
+   new_collection = []
+   while i <array.length
+      new_collection << yield(array[i])
+     i +=1
+
+   end# code here
+   new_collection
 end
